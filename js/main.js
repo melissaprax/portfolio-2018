c43b38a41ce35527b1422d3d53c0f4f568dae03a
melissaprax <melissaprax@gmail.com>
2018-06-15 23:36:41 -0500

    updated contact page and home page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
     
-
-
+$("#js-mail").on('click', function(event) {
+    event.preventDefault();
+    var email = 'melissaprax@gmail.com';
+    var subject = 'from the website';
+    var emailBody = '';
+    window.location = 'mailto:' + email + '?subject=' + subject + '&body=' +   emailBody;
+});
 
 
 
